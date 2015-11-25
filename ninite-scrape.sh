1a7c9029ad5fd55c2b4c3c56d651d56bf00b6cb9
Michael Ripley <zkxs00@gmail.com>
2015-11-25 16:58:20 -0600

    Made script quieter

--- ninite-scrape.sh
+++ ninite-scrape.sh
@@ -1,5 +1,5 @@
 while read i; do
 	if [ ! -f "wget/ninite $i installer.exe" ]; then
-		wget --no-check-certificate -O "wget/ninite $i installer.exe" "https://ninite.com/$i/ninite.exe"
+		wget -a wget.log -q --no-check-certificate -O "wget/ninite $i installer.exe" "https://ninite.com/$i/ninite.exe"
 	fi
 done <ninite-all.txt
