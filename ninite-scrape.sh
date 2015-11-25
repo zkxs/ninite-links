851b361a4fb705bc9ab0dbb71afc12e32bcfac2a
Michael Ripley <zkxs00@gmail.com>
2015-11-25 16:46:29 -0600

    Added check so as to not overwrite existing files

--- ninite-scrape.sh
+++ ninite-scrape.sh
@@ -1,3 +1,5 @@
 while read i; do
-	wget --no-check-certificate -O "wget/ninite $i installer.exe" "https://ninite.com/$i/ninite.exe"
+	if [ ! -f "wget/ninite $i installer.exe" ]; then
+		wget --no-check-certificate -O "wget/ninite $i installer.exe" "https://ninite.com/$i/ninite.exe"
+	fi
 done <ninite-all.txt
