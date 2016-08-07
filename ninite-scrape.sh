a57c8ee9eb154ca752982abad4a5a849575e477a
Michael Ripley <zkxs00@gmail.com>
2016-08-06 21:27:01 -0500

    Print if we're downloading something

--- ninite-scrape.sh
+++ ninite-scrape.sh
@@ -1,5 +1,6 @@
 while read i; do
 	if [ ! -f "wget/ninite $i installer.exe" ]; then
+		echo "Downloading $i..."
 		wget -a wget.log -q --no-check-certificate -O "wget/ninite $i installer.exe" "https://ninite.com/$i/ninite.exe"
 	fi
 done <ninite-all.txt
